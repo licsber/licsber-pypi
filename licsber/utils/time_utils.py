ddfd330f46dee92f40e6a57f3d814148f02a43d5
licsber <silverwings@foxmail.com>
2020-12-21 15:23:51 +0800

    0.0.11 修改时间戳返回值类型.

--- licsber/utils/time_utils.py
+++ licsber/utils/time_utils.py
@@ -10,10 +10,10 @@ def get_now_date() -> str:
     return time.strftime('%Y-%m-%d', now_time)
 
 
-def get_timestamp() -> str:
+def get_timestamp() -> int:
     """
     获取1970年到现在的秒数.
     :return: 自1970年1月1日 0点0分0秒以来的秒数.
     """
     now_time = time.time()
-    return str(int(now_time))
+    return int(now_time)
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='licsber',
-    version='0.0.10',
+    version='0.0.11',
     author='Licsber',
     author_email='licsber@gmail.com',
     url='https://www.cnblogs.com/licsber/',
